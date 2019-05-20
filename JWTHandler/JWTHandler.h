60fa9d7245bdf2521c14e8f5e082542e5e97d3b5
Djordje Ljubinkovic <djordje.ljubinkovic@symphony.is>
2019-05-20 17:16:52 +0200

    Initial Commit

---
+++ JWTHandler/JWTHandler.h
@@ -0,0 +1,16 @@
+// Project: JWTHandler
+//
+// Created on Monday, May 20, 2019.
+// Copyright © 2019 Dorde Ljubinkovic. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JWTHandler.
+FOUNDATION_EXPORT double JWTHandlerVersionNumber;
+
+//! Project version string for JWTHandler.
+FOUNDATION_EXPORT const unsigned char JWTHandlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JWTHandler/PublicHeader.h>
+
+
